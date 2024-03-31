838d80df57938847602b2cf3bf3b0f16a074c181
Jeffrey Zhou <jeffreymzhou@gmail.com>
2024-03-30 21:03:20 -0700

    Initial Commit

---
+++ ToDoList/Views/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Jeffrey Zhou on 3/30/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world Jeffrey!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
