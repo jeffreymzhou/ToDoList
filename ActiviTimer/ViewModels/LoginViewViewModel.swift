db145644529846119fea9b2cd78a3cdae9f78c57
jeffreymzhou <38663022+jeffreymzhou@users.noreply.github.com>
2025-08-06 22:29:45 -0700

    Add sign-in with Apple

--- ActiviTimer/ViewModels/LoginViewViewModel.swift
+++ ActiviTimer/ViewModels/LoginViewViewModel.swift
@@ -5,13 +5,16 @@
 //  Created by Jeffrey Zhou on 3/30/24.
 //
 
-import FirebaseAuth
 import Foundation
+import FirebaseAuth
+import AuthenticationServices
+import CryptoKit
 
 class LoginViewViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
     @Published var errorMessage = ""
+    @Published var currentNonce: String?
     
     init() {}
     
@@ -33,5 +36,98 @@ class LoginViewViewModel: ObservableObject {
         }
         
         return true
+    }
+    
+    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        print("requesting sign in with apple")
+    }
+    
+    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
+        print("handling sign in with apple completion result")
+        switch result {
+        case .success(let authorization):
+            guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+                print("Authorization credential is not of type ASAuthorizationAppleIDCredential")
+                return
+            }
+            guard let nonce = currentNonce else {
+                print("Invalid state: A login callback was received, but no login request was sent.")
+                return
+            }
+            guard let appleIDToken = appleIDCredential.identityToken,
+                    let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to fetch identity token")
+                return
+            }
+
+            let credential = OAuthProvider.credential(
+                withProviderID: "apple.com",
+                idToken: idTokenString,
+                rawNonce: nonce
+            )
+            Auth.auth().signIn(with: credential) {
+                (authResult, error) in
+                if error != nil {
+                    print("received error from firebase auth")
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(
+                        error?.localizedDescription
+                            as Any
+                    )
+                    return
+                }
+                guard let email = Auth.auth().currentUser?.email else {
+                    print("signed in user email: nil")
+                    return
+                }
+                print("signed in user email: \(email)")
+            }
+        case .failure(let error):
+            print("Sign in with Apple failed: \(error.localizedDescription)")
+        }
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(
+            kSecRandomDefault,
+            randomBytes.count,
+            &randomBytes
+        )
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+
+        let charset: [Character] =
+            Array(
+                "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._"
+            )
+
+        let nonce = randomBytes.map { byte in
+            // Pick a random character from the set, wrapping around if needed.
+            charset[Int(byte) % charset.count]
+        }
+
+        return String(nonce)
+    }
+
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
     }
 }
--- ActiviTimer/Views/LoginView.swift
+++ ActiviTimer/Views/LoginView.swift
@@ -5,56 +5,76 @@
 //  Created by Jeffrey Zhou on 3/30/24.
 //
 
+import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 import SwiftUI
 
 struct LoginView: View {
     @StateObject var viewModel = LoginViewViewModel()
-    
+
     var body: some View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView(title: "Activitimer",
-                           subtitle: "Time your Activities",
-                           angle: 15,
-                           background: .pink)
-                
+                HeaderView(
+                    title: "Activitimer",
+                    subtitle: "Time your Activities",
+                    angle: 15,
+                    background: .pink
+                )
+
                 // Login form
                 Form {
                     if !viewModel.errorMessage.isEmpty {
                         Text(viewModel.errorMessage)
                             .foregroundColor(Color.red)
                     }
-                    
+
                     TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
-                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                        .autocapitalization( /*@START_MENU_TOKEN@*/
+                            .none /*@END_MENU_TOKEN@*/
+                        )
                         .autocorrectionDisabled()
-                    
+
                     TextField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
-                    
+
                     TLButton(
                         title: "Log In",
                         background: .blue
                     ) {
                         viewModel.login()
                     }
-                }.offset(y:-60)
-                
+
+                    SignInWithAppleButton(
+                        onRequest: { request in
+                            viewModel.handleSignInWithAppleRequest(request)
+                        },
+                        onCompletion: { result in
+                            viewModel.handleSignInWithAppleCompletion(result)
+                        }
+                    )
+
+                }.offset(y: -60)
+
                 // Create Account
-                VStack{
+                VStack {
                     Text("New around here?")
-    
-                    NavigationLink("Create An Account", destination: RegisterView())
+
+                    NavigationLink(
+                        "Create An Account",
+                        destination: RegisterView()
+                    )
                 }
                 .padding(.bottom, 50)
-                
-                
+
                 Spacer()
             }
         }
     }
+
 }
 
 #Preview {
